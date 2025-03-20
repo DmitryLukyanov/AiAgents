0362fb6dc081bd8d9a2fb7d833a5846323c7d174
DmitryLukyanov <dzima.lukyanau@gmail.com>
2025-03-20 23:31:53 +0400

    Very initial commit

---
+++ AiAgents/DotNetPlugin.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.SemanticKernel;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace AiAgents
+{
+    public sealed class DotNetPlugin
+    {
+        //private readonly IPromptTemplateFactory _promptTemplateFactory = new KernelPromptTemplateFactory();
+        // TODO: take this as arguments
+        private const string GeneratedCodeDirectory = "../../../GeneratedCode";
+        private readonly string GeneratedCodeOutputDirectory = Path.Combine(GeneratedCodeDirectory, "output_project");
+        private const string ClassLibraryName = "class_lib";
+
+        [KernelFunction("CreateCsharpApp"), Description("Create an empty .net/c# class library")]
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
+        public async Task<string> CreateNetAppAsync(KernelArguments arguments)
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
+        {
+            // var renderedPrompt = await _promptTemplateFactory.Create(new PromptTemplateConfig(template: prompt)).RenderAsync(kernel, arguments);
+            using var process = new Process();
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet.exe",
+                Arguments = $"new classlib -f net8.0 -n {ClassLibraryName} -o output_project --force",
+                WorkingDirectory = GeneratedCodeDirectory
+            };
+            process.StartInfo = startInfo;
+            process.Start();
+
+            return $"{ClassLibraryName} has been created";
+        }
+
+        [KernelFunction("SaveCsharpContent"), Description("Save the generated c# method into file")]
+        public async Task<string> DevelopContentOfTheApplication(KernelArguments arguments)
+        {
+            var content = arguments["content"];
+            if (!Directory.Exists(GeneratedCodeDirectory))
+            {
+                return "Error: The empty project must be created before this step";
+            }
+            var class1 = Path.Combine(GeneratedCodeOutputDirectory, "class1.cs");
+            if (!File.Exists(class1))
+            {
+                return "Error: The empty project must be created before this step";
+            }
+            File.WriteAllText(class1, content!.ToString());
+            return "The created method has been successfully saved";
+        }
+
+        [KernelFunction("TestCsharpApplication"), Description("Test a previouly created class library via building project")]
+        public async Task<string> TestApplication(KernelArguments arguments)
+        {
+            if (!Directory.Exists(GeneratedCodeDirectory))
+            {
+                return "Error: The empty project must be created before this step";
+            }
+            var class1 = Path.Combine(GeneratedCodeOutputDirectory, "class1.cs");
+            if (!File.Exists(class1))
+            {
+                return "Error: The empty project must be created before this step";
+            }
+            try
+            {
+                using var process = new Process();
+                var startInfo = new ProcessStartInfo
+                {
+                    FileName = "dotnet.exe",
+                    Arguments = $"build",
+                    WorkingDirectory = GeneratedCodeOutputDirectory,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+                process.StartInfo = startInfo;
+                process.Start();
+                process.WaitForExit(TimeSpan.FromMinutes(1));
+                var output = await process.StandardOutput.ReadToEndAsync();
+                if (output.Contains("Build succeeded"))
+                {
+                    return $"Building of {ClassLibraryName} has been successfully tested";
+                }
+                else
+                {
+                    return $"Failed: {ClassLibraryName} testing has been failed";
+                }
+            }
+            catch (Exception ex) 
+            {
+                return $"Failed testing: {ClassLibraryName} failed the test procedure with this error: {ex.Message}";
+            }
+        }
+    }
+}
---
+++ AiAgents/Program.cs
@@ -0,0 +1,132 @@
+﻿using AiAgents;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Agents.Chat;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        // Define the agent names for use in the function template
+        const string DeveloperName = "Developer";
+        const string TesterName = "Tester";
+
+        var apiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY") ?? throw new InvalidOperationException("OPENAI_API_KEY must be configured");
+
+        var kernelBuilder = Kernel.CreateBuilder();
+        kernelBuilder.AddOpenAIChatCompletion("gpt-4o-mini", apiKey);
+        var kernel = kernelBuilder.Build();
+        kernel.Plugins.AddFromType<DotNetPlugin>(pluginName: nameof(DotNetPlugin), serviceProvider: null);
+
+        var kernelArguments = new KernelArguments(
+            new PromptExecutionSettings()
+            {
+                FunctionChoiceBehavior = FunctionChoiceBehavior.Auto(),
+                // TODO: investigate why this doesn't work
+                ExtensionData = new Dictionary<string, object>() { { "projectName", "ai_output_lib" } }
+            });
+        // TODO: investigate why this doesn't work
+        // kernelArguments.Add("projectName", "ai_output_lib");
+
+        // Create the agents
+        ChatCompletionAgent developerAgent =
+            new()
+            {
+                Name = DeveloperName,
+                Instructions = "Create a C# project with name {{$projectName}}. Then develop method that calculate factorial of provided number. Save the created method in the csharp file",
+                Kernel = kernel,
+                Arguments = kernelArguments
+            };
+
+        ChatCompletionAgent testerAgent =
+            new()
+            {
+                Name = TesterName,
+                Instructions = "Test that the provided method can calcualte the factorial of 10",
+                Kernel = kernel,
+                Arguments = kernelArguments
+            };
+
+        // Define a kernel function for the selection strategy
+#pragma warning disable SKEXP0110 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        KernelFunction selectionFunction =
+            AgentGroupChat.CreatePromptFunctionForStrategy(
+                $$$"""
+        Determine which participant takes the next turn in a conversation based on the the most recent participant.
+        State only the name of the participant to take the next turn.
+        No participant should take more than one turn in a row.
+
+        Choose only from these participants:
+        - {{{TesterName}}}
+        - {{{DeveloperName}}}
+
+        Always follow these rules when selecting the next participant:
+        - After {{{DeveloperName}}}, it is {{{TesterName}}}'s turn.
+        - After {{{TesterName}}}, it is {{{DeveloperName}}}'s turn.
+
+        History:
+        {{$history}}
+        """,
+                safeParameterNames: "history");
+
+        // Define the selection strategy
+#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+        KernelFunctionSelectionStrategy selectionStrategy =
+          new(selectionFunction, kernel)
+          {
+              // Always start with the writer agent.
+              InitialAgent = developerAgent,
+              // Parse the function response.
+              ResultParser = (result) => result.GetValue<string>() ?? DeveloperName,
+              // The prompt variable name for the history argument.
+              HistoryVariableName = "history",
+              // Save tokens by not including the entire history in the prompt
+              HistoryReducer = new ChatHistoryTruncationReducer(3),
+          };
+
+
+        // Define a kernel function for the selection strategy
+        KernelFunction terminationFunction =
+            AgentGroupChat.CreatePromptFunctionForStrategy(
+                $$$"""
+        Determine if the testing has been successfull.  If so, respond with a single word: yes
+
+        History:
+        {{$history}}
+        """,
+                safeParameterNames: "history");
+        // Define the termination strategy
+        KernelFunctionTerminationStrategy terminationStrategy =
+          new(terminationFunction, kernel)
+          {
+              // Only the reviewer may give approval.
+              Agents = [testerAgent],
+              // Parse the function response.
+              ResultParser = (result) =>
+                result.GetValue<string>()?.Contains("yes", StringComparison.OrdinalIgnoreCase) ?? false,
+              // The prompt variable name for the history argument.
+              HistoryVariableName = "history",
+              // Save tokens by not including the entire history in the prompt
+              HistoryReducer = new ChatHistoryTruncationReducer(1),
+              // Limit total number of turns no matter what
+              MaximumIterations = 10,
+          };
+#pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
+        // Create a chat using the defined selection strategy.
+        AgentGroupChat chat =
+            new(developerAgent, testerAgent)
+            {
+                ExecutionSettings = new() { SelectionStrategy = selectionStrategy, TerminationStrategy = terminationStrategy }
+            };
+        chat.AddChatMessage(new ChatMessageContent(AuthorRole.Developer, "Make sure that the generated code follow all based practices"));
+        // invoke
+        await foreach (ChatMessageContent response in chat.InvokeAsync())
+        {
+            Console.WriteLine("\n-----------------------------------------------------------");
+            Console.WriteLine($"{response.Role}: {response.Content}");
+            Console.WriteLine("============================================================\n");
+        }
+    }
+}
